24cf3b0bb8605a69bddb0702d2696bc6d275a60a
SamJones329 <55857337+SamJones329@users.noreply.github.com>
2023-03-30 02:37:56 -0500

    trying to fix fontawesome stuff so its packaged with build

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
 	import '../app.css';
+	import '@fortawesome/fontawesome-free/js/regular.min.js';
+	import '@fortawesome/fontawesome-free/js/solid.min.js';
+	import '@fortawesome/fontawesome-free/js/fontawesome.min.js';
 </script>
 
 <slot />
