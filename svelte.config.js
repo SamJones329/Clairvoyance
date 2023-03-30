1be4617f5868f54e85658bc9a3f923eab390f118
SamJones329 <55857337+SamJones329@users.noreply.github.com>
2023-03-30 02:16:45 -0500

    whoops

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { preprocess } from 'svelte-preprocess';
+import preprocess from 'svelte-preprocess';
 
 // SvelteKit Static Adapter Config
 /** @type {import('@sveltejs/kit').Config} */
